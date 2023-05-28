4e8b58063b6fab50e1c5682e85437a336f2e5a17
messeili <messeili@beamwallet.me>
2023-05-28 12:19:16 +0300

    chore: update next.config.js to route to /search by default

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+require('dotenv').config();
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/search',
+                permanent: true,
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
