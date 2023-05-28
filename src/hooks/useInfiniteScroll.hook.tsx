c00d8cd59dc32144463b2d4d875ebce7b62095e6
messeili <messeili@beamwallet.me>
2023-05-28 12:32:11 +0300

    feat(Hooks): add infinite scrolling hook

---
+++ src/hooks/useInfiniteScroll.hook.tsx
@@ -0,0 +1,23 @@
+import {useEffect} from 'react';
+import {customDebounce} from "@/utils/debounce";
+
+const useInfiniteScroll = (onScrollEnd: Function) => {
+    useEffect(() => {
+        const handleScroll = customDebounce(() => {
+            const {scrollTop, scrollHeight, clientHeight} = document.documentElement;
+            const scrollThreshold = (scrollHeight - clientHeight)  - (scrollHeight * 0.15); // 15% of the scrollHeight
+
+            if (scrollTop >= scrollThreshold) {
+                onScrollEnd();
+            }
+        }, 500);
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [onScrollEnd]);
+};
+
+export default useInfiniteScroll;
