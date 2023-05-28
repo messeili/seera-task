69c42c68922881aaa38079b3309c28e0c28543f9
messeili <messeili@beamwallet.me>
2023-05-28 15:34:30 +0300

    update tests

--- __tests__/githubRepoCard.test.js
+++ __tests__/githubRepoCard.test.js
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import RepositoryCard from "../src/components/repositoryCard";
 
-describe('RepositoryCard', () => {
     const mockProps = {
         name: 'example-repo',
         language: 'JavaScript',
@@ -11,39 +10,49 @@ describe('RepositoryCard', () => {
         forks_count: 10,
         topics: ['topic1', 'topic2', 'topic3', 'topic4', 'topic5'],
     };
-
+describe('RepositoryCard', () => {
     test('renders the repository name', () => {
+        // Arrange
         render(<RepositoryCard {...mockProps} />);
-
+        // Act
         const nameElement = screen.getByTestId('repo-name');
+        // Assert
         expect(nameElement).toBeInTheDocument();
     });
 
     test('renders the repository language', () => {
+        // Arrange
         render(<RepositoryCard {...mockProps} />);
-
+        // Act
         const languageElement = screen.getByTestId('repo-lang');
+        // Assert
         expect(languageElement).toBeInTheDocument();
     });
 
     test('renders the repository description', () => {
+        // Arrange
         render(<RepositoryCard {...mockProps} />);
-
+        // Act
         const descriptionElement = screen.getByTestId('repo-dec');
+        // Assert
         expect(descriptionElement).toBeInTheDocument();
     });
 
     test('renders the repository forks count', () => {
+        // Arrange
         render(<RepositoryCard {...mockProps} />);
-
+        // Act
         const forksElement = screen.getByTestId('repo-forks-count');
+        // Assert
         expect(forksElement).toBeInTheDocument();
     });
 
     test('renders up to three topics', () => {
+        // Arrange
         render(<RepositoryCard {...mockProps} />);
-
+        // Act
         const topics = screen.getByTestId('repo-topics').children;
+        // Assert
         expect(topics).toHaveLength(4);
     });
 });
--- __tests__/githubUserCard.test.js
+++ __tests__/githubUserCard.test.js
@@ -1,30 +1,40 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom'
 import GithubUserCard from "../src/components/githubUserCard";
 
+const mockProps = {
+    name: 'John Doe',
+    avatar: 'https://example.com/avatar.jpg',
+    url: 'https://github.com/johndoe',
+};
 describe('GithubUserCard', () => {
-    const mockProps = {
-        name: 'John Doe',
-        avatar: 'https://example.com/avatar.jpg',
-        url: 'https://github.com/johndoe',
-    };
+    test('renders the name', () => {
+        // Arrange
+        render(<GithubUserCard {...mockProps}/>);
+        // Act
+        const nameElement = screen.getByTestId('user-name')
+        // Assert
+        expect(nameElement).toBeInTheDocument();
+    });
 
     test('renders the name and avatar', () => {
+        // Arrange
         render(<GithubUserCard {...mockProps}/>);
 
-        const nameElement = screen.getByTestId('user-name')
-        expect(nameElement).toBeInTheDocument();
-
+        // Act
         const avatarElement = screen.getByAltText(mockProps.name);
+        // Assert
         expect(avatarElement).toBeInTheDocument();
         expect(avatarElement).toHaveAttribute('src', mockProps.avatar);
     });
 
     test('renders the "Go to Profile" button with the correct URL', () => {
+        // Arrange
         render(<GithubUserCard {...mockProps} />);
-
+        // Act
         const buttonElement = screen.getByTestId('profile-button');
+        // Assert
         expect(buttonElement).toBeInTheDocument();
         expect(buttonElement).toHaveAttribute('href', mockProps.url);
     });
