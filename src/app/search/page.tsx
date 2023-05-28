e6f63adab5020b1762bca8f8a83eb32f3f3e710e
messeili <messeili@beamwallet.me>
2023-05-28 15:38:32 +0300

    refactore(SearchCompoent): update search and fix bugs

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, {useRef, useState} from "react";
+import React, {useCallback, useRef, useState} from "react";
 import {observer} from "mobx-react-lite";
 import {
     Container,
@@ -14,19 +14,26 @@ import UsersResults from "@/components/usersResults";
 import RepoResults from "@/components/repoResults";
 import {userStore} from "@/store/users";
 import {repositoryStore} from "@/store/repositories";
+import ErrorComponent from "@/components/error";
+import SkeletonResults from "@/components/skeletonResults";
 
 const SearchForm: React.FC = observer(() => {
     const [searchType, setSearchType] = useState(SearchTypeConstants.users);
     const [searchQuery, setSearchQuery] = useState('');
     const searchLabel = searchType === SearchTypeConstants.repositories ? 'Search Repositories' : 'Search Users';
     const isInitialSearchState = useRef(true);
+    const getStore = () => {
+        if (searchType === SearchTypeConstants.users) {
+            return userStore
+        } else if (searchType === SearchTypeConstants.repositories) {
+            return repositoryStore
+        } else {
+            throw new Error('Invalid search type')
+        }
+    }
 
     const handleScrollEnd = () => {
-        if (searchType === SearchTypeConstants.users) {
-            userStore.getUsers(searchQuery);
-        } else if (searchType === SearchTypeConstants.repositories) {
-            repositoryStore.getRepositories(searchQuery);
-        }
+        getStore().search(searchQuery);
     };
 
     useInfiniteScroll(handleScrollEnd);
@@ -35,28 +42,17 @@ const SearchForm: React.FC = observer(() => {
         isInitialSearchState.current = true;
         setSearchType(value);
         setSearchQuery('');
-        if (searchType === SearchTypeConstants.users) {
-            userStore.reset();
-        } else if (searchType === SearchTypeConstants.repositories) {
-            repositoryStore.reset();
-        }
+        getStore().reset();
     };
 
-    const handleSearchQueryChange = customDebounce((value: string) => {
-        if (searchType === SearchTypeConstants.users) {
-            userStore.getUsers(value);
-        } else if (searchType === SearchTypeConstants.repositories) {
-            repositoryStore.getRepositories(value);
-        }
-    }, 500);
+    const handleSearchQueryChange = useCallback(customDebounce((value: string) => {
+        getStore().search(value);
+    }, 500), [searchType]);
+
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         isInitialSearchState.current = false;
-        if (searchType === SearchTypeConstants.users) {
-            userStore.reset();
-        } else if (searchType === SearchTypeConstants.repositories) {
-            repositoryStore.reset();
-        }
+        getStore().reset();
         setSearchQuery(event.target.value);
         handleSearchQueryChange(event.target.value);
     };
@@ -90,7 +86,15 @@ const SearchForm: React.FC = observer(() => {
             </form>
             {!isInitialSearchState.current &&
                 <>
-                    {searchType === SearchTypeConstants.users ? <UsersResults/> : <RepoResults/>}
+                    {searchType === SearchTypeConstants.users ?
+                        <UsersResults>{getStore().loading && <SkeletonResults size={30}/>}</UsersResults> :
+                        <RepoResults>{getStore().loading && <SkeletonResults size={30}/>}</RepoResults>}
+                    {getStore().data.length === 0 && !getStore().loading && !getStore().error &&
+                        <div className="flex items-center justify-center mt-20">
+                            <h2 className="text-3xl font-bold text-white">No results found</h2>
+                        </div>
+                    }
+                    {getStore().error && <ErrorComponent message="Error in fetching data"/>}
                 </>}
 
         </Container>
